2b09ec83a3b5f45eb287c38afce471d29c546bed
Victo <victor.salvatierra@outlook.com>
2019-02-22 14:57:32 -0600

    Admin Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
 users = User.create([
-   {name: 'Víctor Salvatierra Mora', email: 'victor.salvatierra@monkeylabs.cr', password: '1234asdf', password_confirmation: '1234asdf', role: 0},
-   {name: 'Víctor Salvatierra Mora', email: 'victor.salvatierra@outlook.com',   password: '1234asdf', password_confirmation: '1234asdf', role: 1},
-   {name: 'Víctor Salvatierra Mora', email: 'vic3x94@gmail.com',                password: '1234asdf', password_confirmation: '1234asdf', role: 2}])
-
-positions = Position.create([{name: 'Oficial', salary: 0}])
-
-bncr_infos = BncrInfo.create([{date: '03021019', company: "18159", transfer_type: "1", consecutive: "1", concept: "PAGO PLANILLA Q1 FEBRERO", account: "100010960008831"}])
-
-bac_infos = BacInfo.create([{date: '20190203', plan: "ADE8", shipping: "00123", concept: "PAGO PLANILLA Q1 FEBRERO"}])
+   {name: 'Usuario Administrador',   email: 'lindsay.ariasb18@gmail.com',       password: 'paseco2019', password_confirmation: 'paseco2019', role: 0},
+   {name: 'Usuario Administrador',   email: 'juspaseco94@gmail.com',            password: 'paseco2019', password_confirmation: 'paseco2019', role: 0},
+   {name: 'Usuario Administrador',   email: 'jariasb91@gmail.com',              password: 'paseco2019', password_confirmation: 'paseco2019', role: 0}])
