71609c28c44714300bf78dad7ade9d4a854a2e24
Victo <victor.salvatierra@outlook.com>
2019-05-13 13:03:02 -0600

    Update roles_controller.rb

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -147,7 +147,7 @@ class RolesController < ApplicationController
       RoleLine.create(role: @role, stall: @stall, employee: @employee, shift: @shift, position: @employee.positions.first)
     end
     
-    @role_lines = @role.role_lines.where(stall_id: @stall.id, employee: @employee).order(id: :asc)
+    @role_lines = @role.role_lines.where(stall_id: @stall.id, employee: @employee).order(date: :asc)
 
     if params[:ajax]
       respond_to do |format|
