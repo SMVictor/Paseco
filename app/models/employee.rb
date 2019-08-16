c09cd173f25c97ef368bdd79a4b26f1814af63be
Victo <victor.salvatierra@outlook.com>
2019-08-16 12:52:07 -0600

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -37,8 +37,8 @@ class Employee < ApplicationRecord
     @used_vacations_days      = 0
     @available_vacations_days = 0
 
-    start_date = Time.now
-    end_date   = Time.now
+    start_date = DateTime.parse(self.entries.last.start_date)
+    end_date   = self.entries.last.end_date != ""? DateTime.parse(self.entries.last.end_date) : Time.now
 
     total_worked_months = -1
 
