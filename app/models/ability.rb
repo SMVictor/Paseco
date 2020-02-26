862700e988fb48729419486c7d7c12fe4ec9dc54
Victo <victor.salvatierra@outlook.com>
2020-02-26 10:21:47 -0600

    Create and Edit employess. Show Payroles and Payrole Details

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,10 @@ class Ability
     elsif user.manager?
       can [:index, :show, :edit, :add_role_lines, :update_role_lines], Role
     elsif user.human_resources?
-      can [:index, :inactives, :show, :show_inactive, :update_vacations, :update_vacations_inactive, :edit_vacations, :edit_vacations_inactive, :vacations_file], Employee
+      can [:index, :inactives, :show, :show_inactive, :update_vacations, :update_vacations_inactive, 
+           :edit_vacations, :edit_vacations_inactive, :vacations_file, :new, :create, :edit, :update, 
+           :edit_inactive, :update_inactive], Employee
+      can [:index_payroles, :show_payroles, :payrole_detail], Role
     else
       can [:index_payroles, :payrole_detail], Role
     end
